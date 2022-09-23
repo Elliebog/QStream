dca0d511187a5288835cff9a1b1cb484caf1e7d1
Elliebird <ellie.bogner@gmail.com>
2022-09-23 21:37:10 +0200

    Inital express setup

---
+++ qstream/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+//set static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  
+  res.sendFile(path.join(__dirname, 'views/overlay.html'));
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
