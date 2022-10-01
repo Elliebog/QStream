ccaeb115fe4ec92ec22647901646bbf50294e6c9
Elliebird <ellie.bogner@gmail.com>
2022-10-01 22:28:55 +0200

    finished navbar

--- qstream/public/js/navbar.js
+++ qstream/public/js/navbar.js
@@ -1,5 +1,20 @@
-$(function () {
+function displaypage(pageid) {
+    //jquery internally checks for class duplicates so no need to check ourselves
     $('.navcontent').addClass('hide');
+    $(`#${pageid}`).removeClass('hide');
+}
+
+$(function () {
+
+    //Setup click events for other navbar items
+    $('#generalnav').click(function (e) { 
+        e.preventDefault();
+        displaypage('generalcontent');
+    });
+
     //hide all navcontent that is not the currently selected one
     //hide everything besides general in the start
+    displaypage('generalcontent');
+
 });
+
